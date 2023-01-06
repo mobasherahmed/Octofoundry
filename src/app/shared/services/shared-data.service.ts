24bbd2caf5308283a6be56a30be9945782786b73
Ahmed Mobasher <Ahmed.Mobasher@geidea.net>
2023-01-06 04:18:45 +0200

    enhance

--- src/app/shared/services/shared-data.service.ts
+++ src/app/shared/services/shared-data.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, of } from 'rxjs';
-import { employess } from '../../mock_data';
+import { employess } from '../mock/mock_data';
 
 
 @Injectable({
